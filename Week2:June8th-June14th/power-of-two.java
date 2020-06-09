aa0a9dc970989cb8ccd94dd0ce2405554d08f458
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-06-08 23:53:58 -0500

    Update power-of-two.java

--- Week2:June8th-June14th/power-of-two.java
+++ Week2:June8th-June14th/power-of-two.java
@@ -34,6 +34,19 @@ class Solution {
 // Approach 2
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        int count=0;
+        while(n>0) {
+            if((n & 1) == 1)
+                count++;
+            n >>= 1;
+        }
+        return count == 1 ? true : false;
+    }
+}
+
+// Approach 3
+class Solution {
+    public boolean isPowerOfTwo(int n) {
         while(n>1) {
             if(n%2 != 0)
                 return false;
